cacb58ee682f2d7766a8988c9ce3f08f1861cf9f
Alexander Bowers <7319abowers@gmail.com>
2020-10-31 23:59:09 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,15 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
+product = nil
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+collection.each do |item|
+  if item[:item] == name
+    product = item
+  end
+end
+product
 end
 
 def consolidate_cart(cart)
@@ -10,8 +17,19 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  receipt = []
+  counter = Hash.new(1)
+  cart.each { |product| receipt << product}
+  receipt.each {|item| counter[item] += 1 }
+  #binding.pry
+  receipt.each do |item|
+    counter.each do |same_item|
+       #binding.pry
+        if item[:item] ==  same_item[0][:item]
+  	         item[:count] = same_item[1]
+        end
+      end
+    end
+  receipt = receipt.uniq
+  receipt
 end
-
-
-  
